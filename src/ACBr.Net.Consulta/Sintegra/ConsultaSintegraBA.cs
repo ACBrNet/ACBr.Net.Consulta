77bf91b262645fc6b1afc0671de5c7a14e27e248
RFTD <rfteno@gmail.com>
2017-07-05 21:37:30 -0400

    [+] Adicionado consulta Sintegra MT e BA. close #7

---
+++ src/ACBr.Net.Consulta/Sintegra/ConsultaSintegraBA.cs
@@ -0,0 +1,156 @@
+﻿// ***********************************************************************
+// Assembly         : ACBr.Net.Consulta
+// Author           : RFTD
+// Created          : 07-05-2017
+//
+// Last Modified By : RFTD
+// Last Modified On : 07-05-2017
+// ***********************************************************************
+// <copyright file="ConsultaSintegraBA.cs" company="ACBr.Net">
+//		        		   The MIT License (MIT)
+//	     		    Copyright (c) 2014 - 2017 Grupo ACBr.Net
+//
+//	 Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//	 The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//	 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using ACBr.Net.Core;
+using ACBr.Net.Core.Exceptions;
+using ACBr.Net.Core.Extensions;
+
+namespace ACBr.Net.Consulta.Sintegra
+{
+    internal class ConsultaSintegraBA : ConsultaSintegraBase<ConsultaSintegraBA>
+    {
+        #region Fields
+
+        private const string URL_CONSULTA = @"http://www.sefaz.ba.gov.br/Sintegra/Result.asp";
+
+        #endregion Fields
+
+        #region Constructors
+
+        public ConsultaSintegraBA()
+        {
+            HasCaptcha = false;
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        public override Image GetCaptcha()
+        {
+            return null;
+        }
+
+        public override ACBrEmpresa Consulta(string cnpj, string ie, string captcha)
+        {
+            var request = GetClient(URL_CONSULTA);
+            request.Method = "POST";
+            var postData = new StringBuilder();
+            postData.AppendFormat("txt_CNPJ={0}&", cnpj.OnlyNumbers());
+            postData.AppendFormat("txt_IE={0}&", ie.OnlyNumbers());
+            postData.Append("EstadoSelecionado=da+Bahia&SiglaEstadoSelecionado=BA");
+
+            var byteArray = ACBrEncoding.ISO88591.GetBytes(postData.ToString());
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = byteArray.Length;
+
+            var dataStream = request.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+
+            var retorno = GetHtmlResponse(request.GetResponse(), Encoding.UTF8);
+
+            Guard.Against<ACBrException>(retorno.Contains("Nenhum resultado encontrado"), $"Não existe no Cadastro do sintegra o número de CNPJ/IE informado.{Environment.NewLine}Verifique se o mesmo foi digitado corretamente.");
+            Guard.Against<ACBrException>(retorno.Contains("a. No momento não podemos atender a sua solicitação. Por favor tente mais tarde."), "Erro no site do sintegra. Tente mais tarde.");
+            Guard.Against<ACBrException>(retorno.Contains("Atenção"), "Erro ao fazer a consulta");
+
+            return ProcessResponse(retorno);
+        }
+
+        #region Private Methods
+
+        /// <summary>
+        /// Processa o retorno html e retorno o objeto tipo ACBrEmpresa com dados
+        /// </summary>
+        /// <param name="retorno"></param>
+        /// <returns>objeto tipo ACBrEmpresa</returns>
+        private static ACBrEmpresa ProcessResponse(string retorno)
+        {
+            var result = new ACBrEmpresa();
+
+            try
+            {
+                var dadosRetorno = new List<string>();
+                dadosRetorno.AddText(retorno.StripHtml());
+                dadosRetorno.RemoveEmptyLines();
+
+                result.CNPJ = LerCampo(dadosRetorno, "CNPJ:");
+                result.InscricaoEstadual = LerCampo(dadosRetorno, "Inscrição Estadual:");
+                result.RazaoSocial = LerCampo(dadosRetorno, "Social:");
+                result.Logradouro = LerCampo(dadosRetorno, "Logradouro:");
+                result.Numero = LerCampo(dadosRetorno, "Número:");
+                result.Complemento = LerCampo(dadosRetorno, "Complemento:");
+                result.Bairro = LerCampo(dadosRetorno, "Bairro:");
+                result.Municipio = LerCampo(dadosRetorno, "Município:");
+                result.UF = (ConsultaUF)Enum.Parse(typeof(ConsultaUF), LerCampo(dadosRetorno, "UF:").ToUpper());
+                result.CEP = LerCampo(dadosRetorno, "CEP:").FormataCEP();
+                result.EndEletronico = LerCampo(dadosRetorno, "Eletrônico:");
+                result.Telefone = LerCampo(dadosRetorno, "Telefone:");
+                result.AtividadeEconomica = LerCampo(dadosRetorno, "Econômica:");
+                result.DataAbertura = LerCampo(dadosRetorno, "da Inscrição Estadual:").ToData();
+                result.Situacao = LerCampo(dadosRetorno, "Situação Cadastral Atual:");
+                result.DataSituacao = LerCampo(dadosRetorno, "desta Situação Cadastral:").ToData();
+                result.RegimeApuracao = LerCampo(dadosRetorno, "de Apuração de ICMS:");
+                result.DataEmitenteNFe = LerCampo(dadosRetorno, "Emitente de NFe desde:").ToData();
+            }
+            catch (Exception exception)
+            {
+                throw new ACBrException(exception, "Erro ao processar retorno.");
+            }
+
+            return result;
+        }
+
+        private static string LerCampo(IList<string> retorno, string campo)
+        {
+            var ret = string.Empty;
+            for (var i = 0; i < retorno.Count; i++)
+            {
+                var linha = retorno[i].Trim();
+                if (linha != campo) continue;
+
+                ret = retorno[i + 1].Trim().Replace("&nbsp;", string.Empty);
+                retorno.RemoveAt(i);
+                break;
+            }
+
+            return ret.RemoveDoubleSpaces();
+        }
+
+        #endregion Private Methods
+
+        #endregion Methods
+    }
+}
---
+++ src/ACBr.Net.Consulta/Sintegra/ConsultaSintegraMT.cs
@@ -0,0 +1,209 @@
+// ***********************************************************************
+// Assembly         : ACBr.Net.Consulta
+// Author           : RFTD
+// Created          : 07-05-2017
+//
+// Last Modified By : RFTD
+// Last Modified On : 07-05-2017
+// ***********************************************************************
+// <copyright file="ConsultaSintegraMT.cs" company="ACBr.Net">
+//		        		   The MIT License (MIT)
+//	     		    Copyright (c) 2014 - 2017 Grupo ACBr.Net
+//
+//	 Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//	 The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//	 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using ACBr.Net.Core;
+using ACBr.Net.Core.Exceptions;
+using ACBr.Net.Core.Extensions;
+
+namespace ACBr.Net.Consulta.Sintegra
+{
+    internal class ConsultaSintegraMT : ConsultaSintegraBase<ConsultaSintegraMT>
+    {
+        #region Fields
+
+        private const string URL_BASE = @"https://www.sefaz.mt.gov.br/sid/consulta/infocadastral/consultar/publica";
+        private const string URL_CAPTCHA = @"https://www.sefaz.mt.gov.br/sid/consulta/geradorcaracteres";
+        private const string URL_CONSULTA = @"https://www.sefaz.mt.gov.br/sid/consulta/infocadastral/consultar/publica";
+
+        #endregion Fields
+
+        #region Methods
+
+        public override Image GetCaptcha()
+        {
+            var request = GetClient(URL_BASE);
+            request.Referer = "https://www.sefaz.mt.gov.br/sid/consulta/infocadastral/consultar/publica";
+
+            var response = request.GetResponse();
+
+            string htmlResult;
+            using (var reader = new StreamReader(response.GetResponseStream()))
+            {
+                htmlResult = reader.ReadToEnd();
+            }
+
+            if (htmlResult.Length < 1) return null;
+
+            var url = $"{URL_CAPTCHA}{htmlResult.GetStrBetween("geradorcaracteres", "\"")}";
+            request = GetClient(url);
+            response = request.GetResponse();
+
+            var captchaStream = response.GetResponseStream();
+            Guard.Against<ACBrCaptchaException>(captchaStream.IsNull(), "Erro ao carregar captcha");
+
+            return Image.FromStream(captchaStream);
+        }
+
+        public override ACBrEmpresa Consulta(string cnpj, string ie, string captcha)
+        {
+            var request = GetClient(URL_CONSULTA);
+            request.Method = "POST";
+            request.Referer = "https://www.sefaz.mt.gov.br/sid/consulta/infocadastral/consultar/publica";
+
+            var postData = new StringBuilder();
+
+            if (!cnpj.IsEmpty())
+            {
+                postData.Append("opcao=2&");
+                postData.Append($"numero={cnpj.OnlyNumbers()}&");
+            }
+            else
+            {
+                postData.Append("opcao=1&");
+                postData.Append($"numero={ie.OnlyNumbers()}&");
+            }
+
+            postData.AppendFormat("captchaDigitado={0}&", captcha);
+            postData.Append("pagn=resultado&");
+            postData.Append("captcha=telaComCaptcha");
+
+            var byteArray = ACBrEncoding.ISO88591.GetBytes(postData.ToString());
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = byteArray.Length;
+
+            var dataStream = request.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+
+            var retorno = GetHtmlResponse(request.GetResponse());
+
+            Guard.Against<ACBrCaptchaException>(retorno.Contains("Código de caracteres inválido!"), "O Texto digitado não confere com a Imagem.");
+            Guard.Against<ACBrException>(retorno.Contains("Nenhum resultado encontrado"), $"Não existe no Cadastro do sintegra o número de CNPJ/IE informado.{Environment.NewLine}Verifique se o mesmo foi digitado corretamente.");
+            Guard.Against<ACBrException>(retorno.Contains("a. No momento não podemos atender a sua solicitação. Por favor tente mais tarde."), "Erro no site do sintegra. Tente mais tarde.");
+            Guard.Against<ACBrException>(retorno.Contains("Atenção"), "Erro ao fazer a consulta");
+
+            return ProcessResponse(retorno);
+        }
+
+        #region Private Methods
+
+        private static ACBrEmpresa ProcessResponse(string retorno)
+        {
+            var result = new ACBrEmpresa();
+
+            try
+            {
+                var dadosRetorno = new List<string>();
+                retorno = HttpUtility.HtmlDecode(retorno);
+                retorno = retorno.StripHtml();
+                dadosRetorno.AddText(retorno.Replace("&nbsp;", Environment.NewLine));
+                dadosRetorno.RemoveEmptyLines();
+
+                result.CNPJ = LerCampo(dadosRetorno, "CPF/CNPJ:");
+                result.InscricaoEstadual = LerCampo(dadosRetorno, "Inscrição estadual:");
+                result.RazaoSocial = LerCampo(dadosRetorno, "Razão social:").Replace("amp;", string.Empty);
+                result.Logradouro = LerCampo(dadosRetorno, "Logradouro:");
+                result.Numero = LerCampo(dadosRetorno, "Número:");
+                result.Complemento = LerCampo(dadosRetorno, "Complemento:");
+                result.Bairro = LerCampo(dadosRetorno, "Bairro:");
+                result.Municipio = LerCampo(dadosRetorno, "Município/UF:");
+                result.UF = (ConsultaUF)Enum.Parse(typeof(ConsultaUF), LerCampo(dadosRetorno, result.Municipio).ToUpper());
+                result.Municipio = result.Municipio.Substring(0, result.Municipio.Length - 2);
+                result.CEP = LerCampo(dadosRetorno, "CEP:").FormataCEP();
+                result.Telefone = LerCampo(dadosRetorno, "Telefone:");
+                result.AtividadeEconomica = LerCampo(dadosRetorno, "Atividade Econômica:");
+                result.DataAbertura = LerCampo(dadosRetorno, "Data de início no Simples Nacional:").ToData();
+                result.Situacao = LerCampo(dadosRetorno, "Situação cadastral atual:");
+                result.DataSituacao = LerCampo(dadosRetorno, "Data desta situação cadastral:").ToData();
+                result.RegimeApuracao = LerCampo(dadosRetorno, "Data desta situação cadastral:");
+                result.DataEmitenteNFe = LerCampo(dadosRetorno, "Emitente de NFe desde:").ToData();
+
+                result.CNAE1 = LerCampo(dadosRetorno, "CNAE Fiscal:");
+                var cnae = LerCampo(dadosRetorno, result.CNAE1);
+                if (cnae != "CNAE Secundário:") result.CNAE1 += $" {cnae}";
+
+                var listCnae2 = new List<string>();
+                var aux = LerCampo(dadosRetorno, "CNAE Secundário:");
+                if (!aux.IsEmpty()) listCnae2.Add(aux);
+
+                do
+                {
+                    aux = LerCampo(dadosRetorno, aux);
+                    if (aux == "Credenciado de ofício como emissor de NF-e:") break;
+
+                    if (!aux.IsEmpty() && char.IsDigit(aux, 0))
+                    {
+                        listCnae2.Add(aux);
+                    }
+                    else
+                    {
+                        listCnae2[listCnae2.Count - 1] += $" {aux}";
+                    }
+                } while (!aux.IsEmpty());
+
+                result.CNAE2 = listCnae2.ToArray();
+            }
+            catch (Exception exception)
+            {
+                throw new ACBrException(exception, "Erro ao processar retorno.");
+            }
+
+            return result;
+        }
+
+        private static string LerCampo(IList<string> retorno, string campo)
+        {
+            var ret = string.Empty;
+            for (var i = 0; i < retorno.Count; i++)
+            {
+                var linha = retorno[i].Trim();
+                if (linha != campo) continue;
+
+                ret = retorno[i + 1].Trim().Replace("&nbsp;", string.Empty);
+                retorno.RemoveAt(i);
+                break;
+            }
+
+            return ret.RemoveDoubleSpaces();
+        }
+
+        #endregion Private Methods
+
+        #endregion Methods
+    }
+}
